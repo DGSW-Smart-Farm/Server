cd4a4c96f10deaf2b89e3e935c2e69c2e52b690e
selenium_tutorial <leatherback___@dgsw.hs.kr>
2021-06-09 16:49:35 +0900

    ADD: models

--- projects/Smart_Farm_Server/v1/models.py
+++ projects/Smart_Farm_Server/v1/models.py
@@ -1,26 +1,27 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
 class sensor(models.Model):
     humidity_gnd_status = models.IntegerField()
     humidity_gnd_value = models.IntegerField()
-    humidity_gnd_time = models.DateTimeField()
+    humidity_gnd_time = models.DateTimeField(default=timezone.now)
 
     humidity_status = models.IntegerField()
     humidity_value = models.IntegerField()
-    humidity_time = models.DateTimeField()
+    humidity_time = models.DateTimeField(default=timezone.now)
 
     fertilizer_status = models.IntegerField()
-    fertilizer_time = models.DateTimeField()
+    fertilizer_time = models.DateTimeField(default=timezone.now)
 
     co2_status = models.IntegerField()
     co2_value = models.IntegerField()
-    co2_time = models.DateTimeField()
+    co2_time = models.DateTimeField(default=timezone.now)
 
     led_status = models.BooleanField()
     led_value = models.IntegerField()
-    led_time = models.DateTimeField()
+    led_time = models.DateTimeField(default=timezone.now)
 
     temp_status = models.IntegerField()
     temp_value = models.IntegerField()
-    temp_time = models.DateTimeField()
+    temp_time = models.DateTimeField(default=timezone.now)
