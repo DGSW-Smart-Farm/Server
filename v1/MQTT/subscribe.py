7d85287aa4be675a0ddb5d22de68f352d34fb814
박준홍 <63082558+Junhong0209@users.noreply.github.com>
2021-09-30 16:05:32 +0900

    Update: 코드 위치 수정

--- v1/MQTT/subscribe.py
+++ v1/MQTT/subscribe.py
@@ -8,19 +8,16 @@ broker = '13.125.136.38'
 def on_message(client, userdata, message):
     global recvData
 
-    time.sleep(1)
     recvData = json.loads(message.payload)
+
+def returnData():
+    return recvData
 
 client = paho.Client()
 client.on_message = on_message
-
 print("connecting to broker ", broker)
 client.connect(broker, 1883)
 print("subscribing ")
 client.subscribe("smartfarm/sensor")#subscribe
 client.loop_start() #start loop to process received messages
 time.sleep(1)
-
-def returnData():
-    return recvData
-print(recvData)
