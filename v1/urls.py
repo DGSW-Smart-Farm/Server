5d27c235051af98def9654bd3c80bf81a0bb08b5
박준홍 <63082558+Junhong0209@users.noreply.github.com>
2021-10-15 14:17:07 +0900

    Delete: 안 쓰는 URL 제거

--- v1/urls.py
+++ v1/urls.py
@@ -4,15 +4,13 @@ from . import views
 app_name = 'v1'
 urlpatterns = [
     path('get_all_sensor/', views.get_all_sensor.as_view(), name='index'),
+    path('humidity/', views.humidity.as_view(), name='index'),
     path('humidity_gnd/', views.humidity_gnd.as_view(), name='index'),
     path('get_home/', views.get_home.as_view(), name='index'),
+    path('temp/', views.temp.as_view(), name='index'),
+    path('air/', views.air.as_view(), name='index'),
     path('led/', views.led.as_view(), name='index'),
     path('fan/', views.fan.as_view(), name='index'),
-    path('temp/', views.temp.as_view(), name='index'),
-    path('humidity_gnd/', views.humidity_gnd.as_view(), name='index'),
-    path('humidity/', views.humidity.as_view(), name='index'),
-    path('air/', views.air.as_view(), name='index'),
-    path('control_water/', views.control_water.as_view(), name='index'),
     path('control_led/', views.control_led.as_view(), name='index'),
     path('control_fan/', views.control_fan.as_view(), name='index'),
 ]
